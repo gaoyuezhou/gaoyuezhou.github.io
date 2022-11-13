3a8e3e4122dfbb386e807be52398c1c0a31e5100
gaoyuezhou <zhougy_99@berkeley.edu>
2022-11-13 17:19:26 -0500

    remove duplicates

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import glob
-# for path in sorted(glob.glob("/Users/gaoyuezhou/dev/photorama/img/galleries/g04/*"), key=str.casefold):
-for path in glob.glob("/Users/gaoyuezhou/dev/photorama/img/galleries/g06/*"):
+for path in sorted(glob.glob("/Users/gaoyuezhou/dev/photorama/img/galleries/g04/*"), key=str.casefold):
+# for path in glob.glob("/Users/gaoyuezhou/dev/photorama/img/galleries/g04/*"):
     img_name = path.split('photorama')[-1]
     print('- image_path: {}'.format(img_name))
     # print('  caption: ')
